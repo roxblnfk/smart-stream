58f07d18c58d4c529cb69dc6f18452f125ad482b
roxblnfk <roxblnfk@ya.ru>
2020-04-22 21:20:03 +0300

    Added WebTemplateBucket

---
+++ src/Data/WebTemplateBucket.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace roxblnfk\SmartStream\Data;
+
+class WebTemplateBucket extends DataBucket
+{
+    private ?string $template = null;
+    private ?string $layout = null;
+    private array $layoutData = [];
+    private array $templateData = [];
+    private array $defaultData = [];
+
+    public function getTemplate(): ?string
+    {
+        return $this->template;
+    }
+    public function getDefaultData(): array
+    {
+        return $this->defaultData;
+    }
+    public function getLayout(): ?string
+    {
+        return $this->layout;
+    }
+    public function getLayoutData(): array
+    {
+        return $this->layoutData;
+    }
+    public function getTemplateData(): array
+    {
+        return $this->templateData;
+    }
+    public function setLayout(string $layout, array $layoutData = []): self
+    {
+        $this->layout = $layout;
+        $this->layoutData = $layoutData;
+        return $this;
+    }
+    public function setTemplate(string $template, array $templateData = []): self
+    {
+        $this->template = $template;
+        $this->templateData = $templateData;
+        return $this;
+    }
+    public function setDefaultData(array $defaultData): self
+    {
+        $this->defaultData = $defaultData;
+        return $this;
+    }
+    public function addDefaultData(string $key, $value): self
+    {
+        $this->defaultData[$key] = $value;
+        return $this;
+    }
+}
